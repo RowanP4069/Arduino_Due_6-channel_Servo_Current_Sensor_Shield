f07ae33823f1fdcc4019f6794eb7c97056a3e5c3
Rowan Patterson <86040017+RowanP4069@users.noreply.github.com>
2022-05-21 11:44:44 +1000

    Add files via upload

--- Arduino Sketch Examples/Sample_1.ino
+++ Arduino Sketch Examples/Sample_1.ino
@@ -3,23 +3,29 @@
 
   Simplest example sketch - Read one sensor only, blocking code (i.e., uses delay() ), unsmoothed (i.e., no noise filtering)
 
-	An Arduino Due sketch to test a custom servo current sensor shield constructed from ACS712 5A current sensors
+  An Arduino Due sketch to test a custom servo current sensor shield constructed from ACS712 5A current sensors
   multiplexed through an 74HC4051 IC. 
 
   Sketch is based on Mux_Analog_Input.ino SparkFun Multiplexer Analog Input Example by Jim Lindblom @ SparkFun Electronics August 15, 2016
   https://github.com/sparkfun/74HC4051_8-Channel_Mux_Breakout
 
-	This library is free software; you can redistribute it and/or
- 	modify it under the terms of the GNU Lesser General Public
- 	License as published by the Free Software Foundation; either
- 	version 2.1 of the License, or (at your option) any later version.
- 	This library is distributed in the hope that it will be useful,
- 	but WITHOUT ANY WARRANTY; without even the implied warranty of
- 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- 	Lesser General Public License for more details.
- 	You should have received a copy of the GNU Lesser General Public
- 	License along with this library; if not, write to the Free Software
- 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
 
   Pin usage:
     Shield JP1+ - Servo 1 connector, power supply / MCU side
@@ -38,7 +44,6 @@
   const int selectPins[3] = {2, 3, 4}; // Which arduino digital pins are connected to 74HC4051 selector pins
   const int zInput = A8; //Which arduino analog pin is connected to 74HC4051 common I/O pin
   const int sampleDelay = 100; //milliseconds between sensor readings
-  const int numSamplesPerSec = int(1000/(sampleDelay));
   const int pin = 1; //Selected sensor for the example
   const int r1 = 2400; //Value of R1 (ohms) in level shift voltage divider
   const int r2 = 4700; //Value of R1 (ohms) in level shift voltage divider
